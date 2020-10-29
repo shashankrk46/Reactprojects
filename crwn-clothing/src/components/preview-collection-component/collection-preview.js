bf02c82f48e3da2fca9955a22b18c3cfe47f1737
shashankrk46 <shashankrk46@gmail.com>
2020-10-29 12:52:00 +0530

    collection-preview and collection-item

--- crwn-clothing/src/components/preview-collection-component/collection-preview.js
+++ crwn-clothing/src/components/preview-collection-component/collection-preview.js
@@ -1,4 +1,6 @@
 import React from 'react';
+
+import CollectionItem from '../collection-item/collection-item'
 import './collection-preview.style.scss'
 
 
@@ -8,8 +10,8 @@ const CollectionPreview = ({ title, items }) => {
       <h1 className='title'>{title.toUpperCase()}</h1>
       <div className='preview'>
         {
-          items.filter((item, idx) => idx < 4).map((item) => (
-            <div key={item.id}>{item.name}</div>
+          items.filter((item, idx) => idx < 4).map(({ id, ...otherItemProps }) => (
+            <CollectionItem key={id} {...otherItemProps} />
           ))
         }
 
