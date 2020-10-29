b34ea41ce94b3dd6a5ac40b23ee4fcda991b8bf3
shashankrk46 <shashankrk46@gmail.com>
2020-10-29 10:13:47 +0530

    added shoppage and collectionpreview

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shopepage/ShopPage';
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    // <div className="App">
+    //   <HomePage />
+    // </div>
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/shop' component={ShopPage} />
+      <Route />
+    </Switch>
   );
 }
 
---
+++ crwn-clothing/src/pages/shopepage/ShopPage.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import SHOP_DATA from './shop.data';
+
+import CollectionPreview from '../../components/preview-collection-component/collection-preview'
+
+class ShopPage extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      collections: SHOP_DATA
+    }
+  }
+
+
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className='shopPage'>
+        {
+          collections.map(({ id, ...otherCollectionProps }) => (
+
+            <CollectionPreview key={id} {...otherCollectionProps} />
+          ))
+        }
+
+      </div>
+    )
+  }
+}
+
+export default ShopPage;
